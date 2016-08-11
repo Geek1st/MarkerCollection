b089278c0a6185894e5ede4d42cf028d25d34cc9
geek1st <geek1st@live.com>
2016-08-11 21:31:09 +0800

    add surefire plugin

---
+++ src/test/java/com/geeklib/util/TestISign.java
@@ -0,0 +1,14 @@
+package com.geeklib.util;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TestISign
+{
+	@Test
+	public void test()
+	{
+		Assert.assertEquals(false, false);
+	}
+}
